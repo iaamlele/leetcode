90645f2d05a34eb85db05ee712ec3934411430dd
Le Chen <LeChen11111@163.com>
2024-06-25 22:15:04 +0800

    ..

--- 69_mySqrt.c
+++ 69_mySqrt.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+
 int mySqrt(int x) {
-    
+    int flag = 0;
+    for(int i = 0; i < (x / 2); i++) {
+        if()
+    }
+}
+
+int main() {
+    int res = mySqrt(8);
+    printf("res:%d\n", res);
+    return 0;
 }
