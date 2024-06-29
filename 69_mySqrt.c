ad7459622ba1700a9742b258662fd2c8d07d774f
Le Chen <LeChen11111@163.com>
2024-06-29 22:58:25 +0800

    upadte

--- 69_mySqrt.c
+++ 69_mySqrt.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
 int mySqrt(int x) {
-    int flag = 0;
-    for(int i = 0; i < (x / 2); i++) {
-        if()
+    long long head = 1;
+    long long tail = x;
+    long long mid;  
+    while(head <= tail) {
+        mid = (head + tail)/2;
+        if(mid * mid < x) {
+            head = mid + 1;
+        }else if(mid * mid > x) {
+            tail = mid - 1;
+        }else if(mid * mid == x) {
+            return mid;
+        }
     }
+    return head - 1;
 }
 
 int main() {
-    int res = mySqrt(8);
+    int res = mySqrt(5);
     printf("res:%d\n", res);
     return 0;
 }
