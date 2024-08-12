141be57e4766ca0309d64d199e0216e564f296ee
Le Chen <LeChen11111@163.com>
2024-08-12 16:48:27 +0800

    update

--- hashtable/15_threeSum_double_point.js
+++ hashtable/15_threeSum_double_point.js
@@ -3,22 +3,33 @@
  * @return {number[][]}
  */
 // 双指针
-// 未去重,怎么去重??
+// 怎么去重??
 var threeSum = function(nums) {
     const result = [];
+    if(nums.length < 3 || nums === null) {
+        return nums;
+    }
     nums = nums.sort((a, b) => a - b);
-    
+    // [-4, -1, -1, 0, 1, 2]
     for(let left = 0; left < nums.length; left++) {
+        if(nums[left] > 0) {
+            break;
+        }
         if(left === 0 || nums[left] !== nums[left - 1]) {
             let right = nums.length - 1;
-            for(let mid = left + 1; mid < nums.length; mid++) {
-                if(mid === left + 1 || nums[mid] !== nums[mid - 1]) {
-                    while(nums[left] + nums[mid] + nums[right] > 0 || right > mid) {
-                        right-=1;
-                    }
-                    if(nums[left] + nums[mid] + nums[right] === 0) {
-                        result.push([nums[left], nums[mid], nums[right]]);
-                    }
+            let mid = left + 1;
+            while(mid < right) {
+                const sum = nums[left] + nums[mid] + nums[right];
+                if(sum === 0) {
+                    result.push([nums[left], nums[mid], nums[right]]);
+                    while(mid < right && nums[mid] === nums[mid + 1]) mid++;
+                    while(mid < right && nums[right] === nums[right - 1]) right--;
+                    mid++;
+                    right--;
+                }else if(sum > 0) {
+                    right--;
+                }else {
+                    mid++;
                 }
             }
         }
