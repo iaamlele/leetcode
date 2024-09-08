02db4339bf9c9e2b5aa7407a6180ed0734f07a90
Le Chen <LeChen11111@163.com>
2024-09-08 08:12:34 +0800

    update

--- binary_tree/654_constructMaximumBinaryTree.js
+++ binary_tree/654_constructMaximumBinaryTree.js
@@ -16,17 +16,29 @@ function TreeNode(val, left, right) {
     this.right = (right === undefined ? null : right);
 }
 
+// 递归法: 前序遍历
+// 注意读题,题中说了nums.length >= 1!
 var constructMaximumBinaryTree = function(nums) {
     const recursion = function(nums) {
+        // 不会写终止条件!!!!终止条件错误,导致栈溢出
         if(nums.length === 0) return null;
+        if(nums.length === 1) return new TreeNode(nums.pop());
+        
+        // Math.max() 函数不能直接处理一个数组，而是需要将数组展开为单个元素传入, 所以不能写Math.max(nums); 要写Math.max(...nums);
+        let max_num = Math.max(...nums);
+        const root = new TreeNode(max_num);
 
-        let max_num = Math.max(nums);
-        // let nums_left = 
+        // Array.prototype.indexOf():返回数组中第一次出现给定元素的下标，如果不存在则返回 -1. 注意!返回的是下标!不是位置!!
+        let determiIndex = nums.indexOf(max_num);
+        
+        let nums_left = nums.slice(0, determiIndex);
+        let nums_right = nums.slice(determiIndex + 1);
 
+        root.left = recursion(nums_left);
+        root.right = recursion(nums_right);
         return root;
     }
 
-    if(!nums) return null;
     return recursion(nums);
 };
 
