706d7d7574c1ad1e82c25ffcf7492b91fe97a105
Le Chen <LeChen11111@163.com>
2024-10-13 23:32:20 +0800

    update

--- monotone_stack/503_nextGreaterElements.js
+++ monotone_stack/503_nextGreaterElements.js
@@ -3,5 +3,20 @@
  * @return {number[]}
  */
 var nextGreaterElements = function(nums) {
+    let sk = [];
+    const len = nums.length;
+    let res= Array(len).fill(-1);
 
+    // 模拟循环
+    for(let i = 0; i < len * 2; i++) {
+        while(sk.length && nums[i % len] > nums[sk[sk.length - 1]]) {
+            let index = sk.pop();
+            res[index] = nums[i % len];
+        }
+        sk.push(i % len);
+    }
+
+    return res;
 };
+
+console.log(nextGreaterElements([1,2,3,4,3]));
