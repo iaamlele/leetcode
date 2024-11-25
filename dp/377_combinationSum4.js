bf657554d90bd55b69c79f9d70309fd903828ac0
Le Chen <LeChen11111@163.com>
2024-11-25 19:12:29 +0800

    finish question 377

--- dp/377_combinationSum4.js
+++ dp/377_combinationSum4.js
@@ -4,5 +4,14 @@
  * @return {number}
  */
 var combinationSum4 = function(nums, target) {
-    
+    let dp = Array(target + 1).fill(0);
+    dp[0] = 1;
+
+    for(let i = 0; i <= target; i++) {
+        for(let j = 0; j < nums.length; j++) {
+            if(i >= nums[j]) {
+                dp[i] += dp[i - nums[j]];
+            }
+        }
+    }
 };
