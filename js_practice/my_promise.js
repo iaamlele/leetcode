0cfa6489bcc90fc035da51fdd70b38442462ad9c
Le Chen <LeChen11111@163.com>
2024-11-29 11:35:49 +0800

    Handwritten promise to complete asynchronous code writing

--- js_practice/my_promise.js
+++ js_practice/my_promise.js
@@ -3,7 +3,7 @@ function MyPromise(executor) {
     // 4.保留this,避免出现this指向不明
     let self = this; 
     // 3. 默认状态是pending, 通过resolve和reject修改状态
-    self.status = 'pending';
+    self.mystatus = 'pending';
     // 6.保存成功/失败回调的值
     self.success = undefined; 
     self.error = undefined;
@@ -12,46 +12,74 @@ function MyPromise(executor) {
     self.onSuccessCallbacks = []; 
     self.onErrorCallbacks = [];
 
-    executor(resolve, reject);
-
     function resolve(success) { 
         // 5.if判断保证 Promise实例状态一旦变更不能再次改变
-        if(self.status === 'pending') {
-            self.status = 'resolved';
+        if(self.mystatus === 'pending') {
+            self.mystatus = 'resolved';
             // 7. 保存最后需要调用的成功/失败的结果
             self.success = success;
-            self.onSuccessCallbacks.push(self.success);
+            // ８.异步：存放成功和失败的回调
+            self.onSuccessCallbacks.forEach(element => {
+                element();
+            })
         }
     }
 
-    function reject(err) {
-        if(self.status === 'pending') {
-            self.status = 'reject';
+    function reject(error) {
+        if(self.mystatus === 'pending') {
+            self.mystatus = 'reject';
             self.error = error;
-            self.onErrorCallbacks.push(self.error);
+            self.onErrorCallbacks.forEach(element => {
+                element();
+            });
         }
     }
 
+    executor(resolve, reject);
 }
 
 // 2.then方法
-MyPromise.prototype.then = (successCallback, failureCallback) => {
+MyPromise.prototype.then = function(successCallback, failureCallback) { //这里应该是普通函数，而不是箭头函数
     let self = this;
-    if(self.status === 'resolved') {
+    if(self.mystatus === 'pending') {
+        self.onSuccessCallbacks.push(() => {
+            successCallback(self.success)
+        });
+        self.onErrorCallbacks.push(() => {
+            failureCallback(self.error)
+        });
+    }
+    if(self.mystatus === 'resolved') {
         successCallback(self.seccess)
     }
-    if(self.status === 'reject') {
+    if(self.mystatus === 'reject') {
         failureCallback(self.error);
     }
 }
 
-let promise = new Promise((resolve, reject) => {
+// 测试用例1: 同步
+// let promise = new Promise((resolve, reject) => {
+//     console.log('start');
+//     resolve('success data');
+// });
+
+// promise.then(res => {
+//     console.log('res', res);
+// }, err => {
+//     console.log('err', err);
+// })
+
+// 测试用例2: 异步
+let promise = new MyPromise((resolve, reject) => {
     console.log('start');
-    resolve('success data');
-});
+    setTimeout(function() {
+        resolve('success data')
+    }, 1000);
+    console.log('end');
+})
 
 promise.then(res => {
     console.log('res', res);
-}, err => {
-    console.log('err', err);
-})
+}, error => {
+    console.log('err', error);
+});
