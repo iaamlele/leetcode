a329346fdcfbd6ce1db0aaea7ae9fdbd531b5851
Le Chen <LeChen11111@163.com>
2024-12-12 16:52:38 +0800

    add Q637

---
+++ binary_tree/second_pass/637_averageOfLevels.js
@@ -0,0 +1,38 @@
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val);
+    this.left = (left === undefined ? null : left);
+    this.right = (right === undefined ? null : right);
+}
+
+var averageOfLevels = function(root) {
+    const res = [];
+    if(!root) return res;
+    const que = [root];
+
+    while(que.length) {
+        const length = que.length;
+        let curSum = 0;
+
+        for(let i = 0; i < length; i++) {
+            const cur = que.shift();
+            curSum += cur.val;
+            cur.left && que.push(cur.left);
+            cur.right && que.push(cur.right);
+        }
+        res.push(curSum / length);
+    }
+    return res;
+}
+
+const root = new TreeNode(3);
+const node2 = new TreeNode(9);
+const node3 = new TreeNode(20);
+root.left = node2;
+root.right = node3;
+
+const node4 = new TreeNode(15);
+const node5 = new TreeNode(7);
+node3.left = node4;
+node3.right = node5;
+
+console.log(averageOfLevels(root));
