18cfa7ad3e1bdc102c2f041355906b2a640f1b7e
Le Chen <LeChen11111@163.com>
2024-12-19 18:13:32 +0800

    update

--- js_practice/second_pass/getOverview.js
+++ js_practice/second_pass/getOverview.js
@@ -10,22 +10,23 @@ let carList = [
 
 const findNode = function(car_info, new_list) {
     if(!new_list.length) return false;
-    for(let i = 0; i < new_list.length; i++) {
-        if(new_list[i].type === car_info.type && new_list[i].status === car_info.status) return new_list[i];
-    }
-    return false;
+    return new_list.find(elem => elem.type === car_info.type && elem.status === car_info.status)
+    // for(let i = 0; i < new_list.length; i++) {
+    //     if(new_list[i].type === car_info.type && new_list[i].status === car_info.status) return new_list[i];
+    // }
 }
 
-const getOverview = function(carList, new_list) {
-    for(let i = 0; i < carList.length; i++) {
-        const find_node = findNode(carList[i], new_list);
+const getOverview = function(carList) {
+    const new_list = [];
+    carList.forEach(element => {
+        const find_node = findNode(element, new_list);
         if(!find_node) {
-            new_list.push({type: carList[i].type, status: carList[i].status, total: 1});
+            new_list.push({type: element.type, status: element.status, total: 1});
         }else {
             find_node.total++;
         }
-    }
+    });
     console.log(new_list);
 }
 
-getOverview(carList, new_list = []);
+getOverview(carList)
